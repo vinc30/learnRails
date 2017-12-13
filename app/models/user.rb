bd81ca6c21963d3dc5ed27da6372dc2d04ea07e3
YuJhengFang <solarorb@gmail.com>
2017-12-13 02:06:14 -0500

    Add a Static Pages controller

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+    has_many :microposts
+    # validates :name, presense: true
+    validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+end
